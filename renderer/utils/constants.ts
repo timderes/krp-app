8b673708a851e44c7929a6862a19e7d87ef213a5
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-09-25 19:20:39 +0200

    Add packet types content
    The game sends these packets to the app

--- renderer/utils/constants.ts
+++ renderer/utils/constants.ts
@@ -3,3 +3,11 @@ export const GAME_STATE = {
   1: "ON_TRACK_PAUSED",
   2: "ON_TRACK",
 };
+
+export const PACKET_TYPES = [
+  "data",
+  "evnt",
+  "lap ", // DO NOT REMOVE THE TRAILING SPACE!
+  "sesn",
+  "splt",
+];
