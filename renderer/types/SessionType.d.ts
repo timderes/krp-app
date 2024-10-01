5dc58fbebb42b2ad33d86f55be0ebfdd6922194a
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-10-01 23:43:19 +0200

    Add helper function to detect the current session type

---
+++ renderer/types/SessionType.d.ts
@@ -0,0 +1,8 @@
+type SessionType =
+  | "IN_GAME_MENU" // `iType` 0
+  | "TESTING" // 1
+  | "RACE" // 2
+  | "CHALLENGE" // 4
+  | "UNKNOWN";
+
+export default SessionType;
---
+++ renderer/utils/ui/getSessionType.ts
@@ -0,0 +1,19 @@
+import type KartEvent from "../../types/KartEvent";
+import type SessionType from "../../types/SessionType";
+
+const getSessionType = (type: KartEvent["m_iType"]): SessionType => {
+  switch (type) {
+    case 0:
+      return "IN_GAME_MENU";
+    case 1:
+      return "TESTING";
+    case 2:
+      return "RACE";
+    case 4:
+      return "CHALLENGE";
+    default:
+      console.warn("Unexpected Session Type:", type);
+      return "UNKNOWN";
+  }
+};
+export default getSessionType;
